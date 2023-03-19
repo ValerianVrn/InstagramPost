72c1ad5ef2ec9461b440bfa1827e885b7d2ecfd0
Valérian VERONA <valerian.verona@fivesgroup.com>
2023-03-19 11:11:51 +0100

    Revert "refactor: hardcoded OpenAI key"
    This reverts commit 0f33abe7e7d6ae18a1e586ff80d170fa2dbbd7b5.

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,9 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
-const OPENAI_API_KEY = "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";
-
 const configuration = new Configuration({
-  apiKey: OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -49,4 +47,17 @@ export default async function (req, res) {
       });
     }
   }
+}
+
+function generatePrompt(animal) {
+  const capitalizedAnimal =
+    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  return [
+    {role: "system", content: "You are a charming Instagramer who posts pastries in nice backgrounds. You have a sense of humour and you are joyful. You use famous hashtags."},
+    {role: "user", content: "Create an Instagram post of an apple pie with a ice cream on sunset. This is the first post of the account."},
+    {role: "assistant", content: "Welcome to my dessert adventure! 🍨🍎 As my first post, I'm sharing my all-time favorite dessert - warm apple pie with a scoop of vanilla ice cream, enjoyed during a beautiful sunset. There's something magical about the combination of sweet and tart flavors with a creamy finish. Who's ready for a slice? 🤤 #applepie #vanillaicecream #sunsetdessert #sweettoothsatisfied #dessertadventure #firstpost"},
+    {role: "user", content: "Generate an Instagram post of chocolate macarons and champagne at Courchevel. Highlight the macaron love and simple pleasure."},
+    {role: "assistant", content: "Life is short, but sweet moments like these are forever. 🍫🥂 There's nothing like indulging in the simple pleasure of biting into a perfectly crafted chocolate macaron, while sipping on a glass of champagne in the heart of Courchevel. ❄️☃️ #macaronlove #sweetmoment #champagnelife #courchevel #indulgence #simplepleasures"},
+    {role: "user", content: `Generate a post about ${capitalizedAnimal}.`}
+];
 }
